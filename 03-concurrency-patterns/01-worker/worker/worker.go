de2a0b34bb0f856dfdddd2f2db5859ccda7f9f78
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-30 17:22:00 +0530

    worker pattern - solution

--- 03-concurrency-patterns/01-worker/worker/worker.go
+++ 03-concurrency-patterns/01-worker/worker/worker.go
@@ -1,17 +1,42 @@
 package worker
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Work interface {
 	Task()
 }
 
-func New() {
-	/*  */
+type Worker struct {
+	workQueue chan Work
+	wg        sync.WaitGroup
 }
 
-func ( /*  */ ) Run() {
-	/*  */
+func New(workersCount int) *Worker {
+	w := &Worker{
+		workQueue: make(chan Work),
+	}
+	w.wg.Add(workersCount)
+	for idx := 0; idx < workersCount; idx++ {
+		go func(id int) {
+			fmt.Printf("Worker %d starts...\n", id)
+			for wk := range w.workQueue {
+				wk.Task()
+			}
+			w.wg.Done()
+		}(idx)
+	}
+	return w
 }
 
-func ( /*  */ ) Shutdown() {
-	/*  */
+func (w *Worker) Run(work Work) {
+	w.workQueue <- work
+}
+
+func (w *Worker) Shutdown() {
+	close(w.workQueue)
+	w.wg.Wait()
+	fmt.Println("Worker shutdown completed")
 }
