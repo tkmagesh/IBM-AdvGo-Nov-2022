4c390e4b6c6308c4d98e4ff3169c2faec805ea19
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-01 17:07:21 +0530

    service & server implementation

---
+++ 04-grpc-app/server/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"grpc-app/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type appServiceImpl struct {
+	proto.UnimplementedAppServiceServer
+}
+
+func (asi *appServiceImpl) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResponse, error) {
+	x := req.GetX()
+	y := req.GetY()
+
+	result := x + y
+
+	res := &proto.AddResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
+func main() {
+	asi := &appServiceImpl{}
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	grpcServer := grpc.NewServer()
+	proto.RegisterAppServiceServer(grpcServer, asi)
+	grpcServer.Serve(listener)
+}
