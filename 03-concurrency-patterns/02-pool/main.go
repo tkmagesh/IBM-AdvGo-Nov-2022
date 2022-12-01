730fb45695a13742bf4de545c52cc3f789e34626
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-01 11:03:41 +0530

    updates

--- 03-concurrency-patterns/02-pool/main.go
+++ 03-concurrency-patterns/02-pool/main.go
@@ -37,6 +37,39 @@ func DBConnectionFactory() (io.Closer, error) {
 }
 
 func main() {
+	/*
+		create a Pool instance (with the pool size, factory function)
+
+		Resource => any object that implements "close()" method (io.Closer interface)
+
+		When a resource is Acquire()d
+			the pool will check if it has any resources
+			if yes, return the resource from the pool
+			else create a new resource (using the factory) and return
+
+		When a resource is Release()d to the pool
+			the pool will check if it is full
+			if yes, then discard the resource (after 'closing' the resource)
+			else, keep the resource to serve future requests (maintain the resource in the pool)
+
+		When Close()ing the pool
+			prevent anymore aqcuisition of the resources
+			make sure all the resources are closed and discarded
+
+		Notes:
+			The number of requests for the resources CAN exceed the pool size
+			When a resource is acquired by the client, the same resource SHOULD NOT be given to another client until the resource is released back to pool
+
+
+		Functions:
+			New(poolSize, factory) => pool
+
+		Methods (pool)
+			Aquire() => resource
+			Release(resource)
+			Close()
+
+	*/
 	p, err := pool.New(5, DBConnectionFactory)
 	if err != nil {
 		log.Fatalln(err)
